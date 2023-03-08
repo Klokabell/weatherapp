644a7f025997e521f374b06de7ed43c5390abc8c
Michael Bell <33485079+Klokabell@users.noreply.github.com>
2023-03-08 13:17:17 +0200

    adding the dropdown and testing vscod/git combo

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
 import './App.css';
 import React from 'react';
 const api = {
-    key:
-    base:
+    key: "77c5e049fe0b5f2b238f8ee7a7c342c8",
+    base:"https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid="
 }
 
 function App() {
   
+
+    return(
+        <main>
+            <div className="city-picker">
+                <p></p>
+                <label htmlFor="cities">Select the city you want to view</label>
+                <select name="cities" id="cities">
+                    <option value="espoo"></option>
+                </select>
+            </div>
+        </main>
+    )
 }
 
 export default App;
