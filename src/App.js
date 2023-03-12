884373784c5615bae025010de3ae46d7464b7f88
Michael Bell <33485079+Klokabell@users.noreply.github.com>
2023-03-12 18:37:41 +0200

    Switching from hardcoded API fetch

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
 import React, { useEffect, useState } from 'react';
 
-
 const api = {
     key: "77c5e049fe0b5f2b238f8ee7a7c342c8",
-    base:"https://api.openweathermap.org/data/2.5/weather?lat=61.4991&lon=23.7871&limit=1&appid="
+    currenturl:"https://api.openweathermap.org/data/2.5/weather?lat=61.4991&lon=23.7871&limit=1&appid=",
+    forecasturl:"https://api.openweathermap.org/data/2.5/forecast?lat=61.4991&lon=23.7871&appid="
 }
 const cities = [
 
@@ -33,13 +33,36 @@ const cities = [
 function App() {
 
     const [weather, setWeather] = useState({})
+    const [forecast, setForecast] = useState({})
+    const [isLoading2, setIsLoading2] = useState(true)
+
     useEffect(() => {
-            fetch(`${api.base}${api.key}&units=metric`)
-            .then(res=>res.json())
-            .then(res => setWeather(res))
+        Promise.all([
+            fetch(`${api.currenturl}${api.key}&units=metric`)
+                .then(res=>res.json())
+                .then(res=>setWeather(res)),
+            fetch(`${api.forecasturl}${api.key}&units=metric`)
+                .then(res=>res.json())
+                .then(res=>{
+                    setForecast(res)
+                    setIsLoading2(false)
+                })
+        ])
     }, [])
 
+    const rainCheck = (raining) =>{
+        if(raining.rain){
+            return raining.rain["1h"] +"mm"
+        }
+        else return "0 mm"
+    }
+    console.log(forecast)
     console.log(weather)
+
+    const weathericon = (icon) => {
+        let iconurl = "https://openweathermap.org/img/wn/"+icon+"@2x.png"
+        return iconurl
+    }
 
   
     const dateMaker = () => {
@@ -48,48 +71,119 @@ function App() {
                       "September", "October", "November", "December"];
         
         let d = new Date()
-        let day = d.getDate() + " "
-        let month = months[d.getMonth()]
+        let day = d.getDate()
+        let month = months[d.getMonth()] + " "
         let padMinutes = String(d.getMinutes()).padStart(2, '0')
-        let time = d.getHours() + ":" + padMinutes
+        let hours = d.getHours() 
+        let time = hours + ":" + padMinutes
 
-        let date = [day, month, time]
+
+        let date = [month, day, time, hours]
         return date
     }
 
     return(
         <div>
+            <div className="banner"></div>
+            <h1>Säätutka</h1>
             <main>
-                <div className="gridcontainer">
+                <div className="gridContainer">
                     <div className="city-picker">
-                        <label htmlFor="cities">Select the city you want to view</label>
-                        <select name="cities" id="cities">
-                            <option value="Tampere" id='0'>Tampere</option>
-                        </select>
+                            <select name="cities" id="cities">
+                                <option value="Tampere" id='0'>Tampere</option>
+                            </select>
                     </div>
-                    {(typeof weather.main != "undefined") ? (
-                    <div className='currentweather'>
-                        <div className='city'>Tampere</div>
-                        <div className="sky">{weather.weather[0].description}</div>
-                        <div className="temperature">{Math.round(weather.main.temp)}</div>
-                        <div className="date">{dateMaker().slice(0,2)}</div>
-                        <div className="time">{dateMaker().slice(2,3)}</div>
-                        <div className="extraweather"></div>
+                    <div className="citygrid">
+                        {(typeof weather.main != "undefined") ? (
+                            <div className='currentweather'>
+                                <div className='grid-item topleft'>
+                                    <div className='city'>{weather.name}</div>
+                                    <div className="sky">{weather.weather[0].description}</div>
+                                </div>
+                                <div className="grid-item topright">
+                                    <div className="icon"><img id='wicon' src={weathericon(weather.weather[0].icon)} alt="icon"></img></div>
+                                    <div className="temperature">{Math.round(weather.main.temp)} °C</div>
+                                </div>
+                                <div className="grid-item botleft">                            
+                                    <div className="date">{dateMaker().slice(0,2)}</div>
+                                    <div className="time">{dateMaker().slice(2,3)}</div>
+                                </div>
+                                <div className="grid-item botright">
+                                    <div className="wind">Wind: {weather.wind.speed} m/s</div>
+                                    <div className="humidity">Humidity: {weather.main.humidity} %</div>
+                                    <div className="precipitation">Precipitation (3h): {rainCheck(weather)}</div>
+                                </div>
+                            </div>
+                            ) : ('')}
+                            {isLoading2 ? (<div>Loading</div>) : 
+                            (
+                            <div className="forecast">
+                                <div className="plus" id='3'>
+                                    <div className="tophalf">
+                                        <div className="time">{(dateMaker()[3] + 3)%24}:00</div>
+                                        <div className="icon"><img id='ficon' src={weathericon(forecast.list[1].weather[0].icon)} alt="icon"></img></div>
+                                        <div className="temperature">{Math.round(forecast.list[1].main.temp)} °C</div>
+                                    </div>
+                                    <div className="bothalf">
+                                        <div className="wind">{forecast.list[1].wind.speed} m/s</div>
+                                        <div className="humidity">{forecast.list[1].main.humidity} %</div>
+                                        <div className="precipitation">mm</div>
+                                    </div>
+                                </div>
+                                <div className="plus" id='6'>
+                                    <div className="tophalf">
+                                        <div className="time">{(dateMaker()[3] + 6)%24}:00</div>
+                                        <div className="icon">{<img id='ficon' src={weathericon(forecast.list[2].weather[0].icon)} alt="icon"></img>}</div>
+                                        <div className="temperature">{Math.round(forecast.list[2].main.temp)} °C</div>
+                                    </div>
+                                    <div className="bothalf">
+                                        <div className="wind">{forecast.list[2].wind.speed} m/s</div>
+                                        <div className="humidity">{forecast.list[2].main.humidity} %</div>
+                                        <div className="precipitation">mm</div>
+                                    </div>
+                                </div>
+                                <div className="plus" id='9'>
+                                    <div className="tophalf">
+                                        <div className="time">{(dateMaker()[3] + 9)%24}:00</div>
+                                        <div className="icon">{<img id='ficon' src={weathericon(forecast.list[3].weather[0].icon)} alt="icon"></img>}</div>
+                                        <div className="temperature">{Math.round(forecast.list[3].main.temp)} °C</div>
+                                    </div>
+                                    <div className="bothalf">
+                                        <div className="wind">{forecast.list[3].wind.speed} m/s</div>
+                                        <div className="humidity">{forecast.list[3].main.humidity} %</div>
+                                        <div className="precipitation">mm</div>
+                                    </div>
+                                </div>
+                                <div className="plus" id='12'>
+                                    <div className="tophalf">                            
+                                        <div className="time">{(dateMaker()[3] + 12)%24}:00</div>
+                                        <div className="icon">{<img id='ficon' src={weathericon(forecast.list[4].weather[0].icon)} alt="icon"></img>}</div>
+                                        <div className="temperature">{Math.round(forecast.list[4].main.temp)} °C</div>
+                                    </div>
+                                    <div className="bothalf">
+                                        <div className="wind">{forecast.list[4].wind.speed} m/s</div>
+                                        <div className="humidity">{forecast.list[4].main.humidity} %</div>
+                                        <div className="precipitation">mm</div>
+                                    </div>
+                                </div>
+                                <div className="plus" id='15'>
+                                    <div className="tophalf">
+                                        <div className="time">{(dateMaker()[3] + 15)%24}:00</div>
+                                        <div className="icon">{<img id='ficon' src={weathericon(forecast.list[5].weather[0].icon)} alt="icon"></img>}</div>
+                                        <div className="temperature">{Math.round(forecast.list[5].main.temp)} °C</div>
+                                    </div>
+                                    <div className="bothalf">
+                                        <div className="wind">{forecast.list[5].wind.speed} m/s</div>
+                                        <div className="humidity">{forecast.list[5].main.humidity} %</div>
+                                        <div className="precipitation">mm</div>
+                                    </div>
+                                </div>
+                            </div> 
+                        )}
                     </div>
-                    ) : ('')}
-
-                    <div className="forecast">
-                        <div className="plus3"></div>
-                        <div className="plus6"></div>
-                        <div className="plus9"></div>
-                        <div className="plus12"></div>
-                        <div className="plus15"></div>
-                    </div>
-                </div>
-
+                </div>   
             </main>
-        </div>
-
+        </div>         
     )
 }
 
